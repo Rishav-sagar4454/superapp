da82521f60e5983424f4361629c86eed426deba6
rishavsagar4454 <rishavsagar4454@gmail.vom>
2023-05-30 23:04:46 +0530

    js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Genre from './pages/Genre'
 
+
+
 const App=()=> {
 
   return (
     <BrowserRouter>
     
-       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/Genre' element={<Genre/>}></Route>
-       </Routes>
-    
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/genre" element={<Genre/>}/>
+      
+      
+    </Routes>
     </BrowserRouter>
   )
 }
