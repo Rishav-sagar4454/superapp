f67dd1dde4f9b473bd31cb9fb3a8af463183ea5a
rishavsagar4454 <rishavsagar4454@gmail.vom>
2023-06-02 18:27:28 +0530

    update

--- src/components/Home/SignUpForm.jsx
+++ src/components/Home/SignUpForm.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import styles from './Signup.css'
+import styles from './Signup.module.css'
 import { useNavigate } from 'react-router-dom'
 import homepage from '../../assests/homepage.png'
 const SignUpForm = ()=>{
